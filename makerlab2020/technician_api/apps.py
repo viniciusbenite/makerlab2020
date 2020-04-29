0b63d79affb94858ec67dd4667469966ba6d8076
Vinícius Ribeiro <viniciusribeiro@ua.pt>
2020-04-29 20:37:36 +0100

    templates

--- makerlab2020/technician_api/apps.py
+++
@@ -1,7 +0,0 @@
-# from django.apps import AppConfig
-from wiki.apps import WikiConfig
-
-
-class TechApiConfig(WikiConfig):
-    name = 'makerlab2020.technician_api'
-    verbose_name = 'DETI MakerLab Technician'
--- makerlab2020/users_api/apps.py
+++
@@ -1,7 +0,0 @@
-# from django.apps import AppConfig
-from wiki.apps import WikiConfig
-
-
-class UserApiConfig(WikiConfig):
-    name = 'makerlab2020.users_api'
-    verbose_name = 'DETI MakerLab Users'
