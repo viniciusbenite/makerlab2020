cc03fef8709fd8a46a5870fb185ac748080c3354
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-04 14:46:17 +0100

    add gesture dectector for about widgets

--- makerlab/lib/screens/about.dart
+++ makerlab/lib/screens/about.dart
@@ -7,17 +7,33 @@ class AboutPage extends StatelessWidget {
     return Center(
       child: ListView(
         children: [
-          CardView(
-            image: 'https://makerlab2020.herokuapp.com/static/img/Makerlab_About.jpg',
-            text: 'What is Makerlab?',
+          GestureDetector(
+            onTap: () {
+              //TODO
+            },
+            child: CardView(
+              image:
+                  'https://makerlab2020.herokuapp.com/static/img/Makerlab_About.jpg',
+              text: 'What is Makerlab?',
+            ),
           ),
-          CardView(
-            image: 'https://makerlab2020.herokuapp.com/static/img/Project_About.jpg',
-            text: 'Project and Team',
+          GestureDetector(
+            onTap: () {
+              //TODO
+            },
+            child: CardView(
+              image:
+                  'https://makerlab2020.herokuapp.com/static/img/Project_About.jpg',
+              text: 'Project and Team',
+            ),
           ),
-          CardView(
-            image: 'https://makerlab2020.herokuapp.com/static/img/Wiki_About.jpg',
-            text: 'About Wiki',
+          GestureDetector(
+            onTap: () {},
+            child: CardView(
+              image:
+                  'https://makerlab2020.herokuapp.com/static/img/Wiki_About.jpg',
+              text: 'About Wiki',
+            ),
           ),
         ],
       ),
