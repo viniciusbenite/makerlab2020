7506498967bb898fa6f1710dcc8725483dcb8d7c
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-05 22:44:26 +0100

    projects done statically

--- makerlab/lib/screens/projects.dart
+++ makerlab/lib/screens/projects.dart
@@ -10,51 +10,72 @@ class Projects extends StatefulWidget {
 class _ProjectsState extends State<Projects> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Table(
-          border: TableBorder.all(),
-          defaultVerticalAlignment: TableCellVerticalAlignment.top,
-          children: [
-            TableRow(
-              decoration:
-                  BoxDecoration(color: Theme.of(context).primaryColorDark),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0)
+            return Column(
               children: [
-                _text('Number', true),
-                _text('Project Name', true),
-                _text('Supervisor', true),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Text(
+                    'My Projects',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+                _tableRow("Number", "Project Name", "Supervisor", index),
               ],
-            ),
-            TableRow(
-              children: [
-                Text('Number'),
-                Text('Project Name'),
-                Text('Supervisor'),
-              ],
-            ),
-            TableRow(
-              children: [
-                Text('Number'),
-                Text('Project Name'),
-                Text('Supervisor'),
-              ],
-            ),
-          ],
-        ),
+            );
+          return _tableRow("text1", "text2", "text3", index);
+        },
       ),
     );
   }
 
-  Widget _text(String text, bool bold) => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: bold? FontWeight.bold: FontWeight.normal,
-            fontSize: 16
+  Widget _tableRow(text1, text2, text3, index) {
+    Color color;
+    if (index == 0) {
+      color = Theme.of(context).primaryColorDark;
+    } else if (index % 2 != 0)
+      color = Theme.of(context).primaryColorLight;
+    else
+      color = Theme.of(context).primaryColor;
+
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: color,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            text1,
+            style: TextStyle(
+              color:
+                  index == 0 || index % 2 == 0 ? Colors.white : Colors.black,
+              fontWeight: index == 0 ? FontWeight.bold : FontWeight.normal,
+            ),
           ),
-        ),
-  );
+          Text(
+            text2,
+            style: TextStyle(
+              color:
+                  index == 0 || index % 2 == 0 ? Colors.white : Colors.black,
+              fontWeight: index == 0 ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+          Text(
+            text3,
+            style: TextStyle(
+              color:
+                  index == 0 || index % 2 == 0 ? Colors.white : Colors.black,
+              fontWeight: index == 0 ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
