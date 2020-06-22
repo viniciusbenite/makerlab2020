ed04b7b1edb11fa6591a264becc273150eb65425
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-22 15:36:40 +0100

    mess around in the main page

--- makerlab/lib/screens/mainPage.dart
+++ makerlab/lib/screens/mainPage.dart
@@ -1,75 +1,77 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:makerlab/widgets/delayed_animations.dart';
 
 class MainScreen extends StatelessWidget {
+  Widget _box(String text) => Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('text'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                padding: EdgeInsets.all(32.0),
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              text,
+              style: TextStyle(fontSize: 12),
+            )
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     //TODO: This page is still a mess, do later
     return Container(
+      height: MediaQuery.of(context).size.height,
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: NetworkImage(
-            'https://cdn.noitesaudavel.pt/news/0000000000109/1568630397.jpg',
-          ),
-          fit: BoxFit.fitHeight,
-        ),
+            image: NetworkImage(
+                'https://makerlab2020.herokuapp.com/static/img/BackgroundMainCropped.png'),
+            fit: BoxFit.cover),
       ),
       child: Column(
         children: [
-          Container(
-            padding: EdgeInsets.all(64.0),
-            child: Center(
-              child: DelayedAnimation(
-                child: Text(
-                  'MAKERLAB',
-                  style: Theme.of(context).textTheme.headline4,
+          SizedBox(
+            height: 35,
+          ),
+          Expanded(
+            child: DelayedAnimation(
+              child: Container(
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  //TODO: Change logo
+                  child: FlutterLogo(
+                    size: 100,
+                  ),
+                  radius: 100.0,
                 ),
-                delay: 500,
               ),
+              delay: 800,
             ),
           ),
           Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    RaisedButton(
-                      onPressed: () {},
-                      child: Container(
-                        padding: EdgeInsets.all(32.0),
-                        child: Text('data'),
-                      ),
-                    ),
-                    RaisedButton(
-                      onPressed: () {},
-                      child: Text('data'),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    RaisedButton(
-                      onPressed: () {},
-                    ),
-                    RaisedButton(
-                      onPressed: () {},
-                    ),
-                    RaisedButton(
-                      onPressed: () {},
-                    ),
-                  ],
-                )
-              ],
+            flex: 2,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 1.5,
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: [
+                  _box('Create Project'),
+                  _box('Rent Equipment'),
+                  _box('Wiki'),
+                  _box('WebPage'),
+                ],
+              ),
             ),
           ),
-          SizedBox(
-            height: 180,
-          )
         ],
       ),
     );
