db8f9500ec182ddc0d50fa42103f1313024ffaa1
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-26 15:12:44 +0100

    add equipment model

---
+++ makerlab/lib/models/equipment.dart
@@ -0,0 +1,16 @@
+class Equipment {
+  int id;
+  String family;
+  String description;
+  int inStock;
+  String status;
+  String details;
+
+  Equipment(
+      {this.id,
+      this.family,
+      this.description,
+      this.inStock,
+      this.status,
+      this.details});
+}
