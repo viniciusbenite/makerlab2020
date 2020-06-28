74be148766f91d5c0b86d6b810f3c8392f538a54
miguelmatos-ua <miguelcruzmatos@ua.pt>
2020-06-28 18:16:34 +0100

    add project model

---
+++ makerlab/lib/models/project.dart
@@ -0,0 +1,18 @@
+class Project {
+  int id;
+  int year;
+  int semester;
+  String projectName;
+  String projectShortName;
+  String supervisor;
+  int numberOfTeamMembers;
+
+  Project(
+      {this.id,
+      this.year,
+      this.semester,
+      this.projectName,
+      this.projectShortName,
+      this.supervisor,
+      this.numberOfTeamMembers});
+}
